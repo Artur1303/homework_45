ee6f2e826170b867cd684cd79d6e827637c9882c
Artur <metis130383@gmail.com>
2020-07-24 16:36:04 +0600

    Создал  модель  задачи

---
+++ source/webapp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+STATUS_CHOICES = [
+    ('new', 'Новая'),
+    ('in_progress', 'В процессе'),
+    ('done', 'Сделано')
+]
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='статус')
+    data = models.DateField(null=True, blank=True, verbose_name='Время завершение')
+
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.title)
+
+    class Meta:
+        verbose_name = 'Задачи'
+        verbose_name_plural = 'Задача'
---
+++ source/webapp/views.py
@@ -0,0 +1,48 @@
+
+from django.shortcuts import render, redirect, get_object_or_404
+from webapp.models import Article, STATUS_CHOICES
+from django.http import HttpResponseNotAllowed, Http404
+
+
+def index_view(request):
+    is_admin = request.GET.get('is_admin', None)
+    if is_admin:
+        data = Article.objects.all()
+    else:
+        data = Article.objects.filter(status='moderated')
+    return render(request, 'index.html', context={
+        'articles': data
+    })
+
+
+def article_view(request, pk):
+    # try:
+    #     article = Article.objects.get(pk=pk)
+    # except Article.DoesNotExist:
+    #     raise Http404
+
+    article = get_object_or_404(Article, pk=pk)
+
+    context = {'article': article}
+    return render(request, 'article_view.html', context)
+
+
+def article_create_view(request):
+    if request.method == "GET":
+        return render(request, 'article_create.html', context={
+            'status_choices': STATUS_CHOICES
+        })
+    elif request.method == 'POST':
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        author = request.POST.get('author')
+        status = request.POST.get('status')
+        article = Article.objects.create(title=title, text=text, author=author, status=status)
+
+        # url = reverse('article_view', kwargs={'pk': article.pk})
+        # return redirect(url)
+        return redirect('article_view', pk=article.pk)
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
+
+
